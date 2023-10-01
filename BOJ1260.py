205f366256d565ffba6ab42f75e0927732754afe
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-10-01 11:29:00 +0900

    DFS, BFS 문제 (제출은 못함)
    출력문중 맨마지막에 "%" 가 같이 출력되어 출력초과로 나타남

---
+++ BOJ1260.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def dfs(idx):
+    global visited
+    visited[idx] = True
+    print(idx, end=' ')
+    for next in range(1, N+1):
+        if not visited[next] and graph[idx][next]:
+            dfs(next)
+
+
+def bfs():
+    global q, visited
+    while q:
+        cur = q.pop(0)
+        visited[cur] = True
+        print(cur, end=' ')
+        for next in range(1, N+1):
+            if not visited[next] and graph[cur][next]:
+                visited[next] = True
+                q.append(next)
+
+
+# 0. 입력 및 초기화
+input = sys.stdin.readline
+N, M, V = map(int, input().split())
+graph = [[False] * (N + 1) for _ in range(N + 1)]
+visited = [False] * (N + 1)
+
+# 1. graph 정보 입력
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+# 2.  DFS
+    dfs(V)
+    print()
+
+# 4. BFS
+visited = [False] * (N + 1)
+q = [V]
+bfs()
