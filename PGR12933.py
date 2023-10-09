3a1e87619fd268c22e30b3dc072c4cf8ef958cfc
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-10-10 08:24:58 +0900

    TIL day 6
    Programmers 연습문제 Lv.1

--- PGR12933.py
+++ PGR12933.py
@@ -4,14 +4,15 @@ https://school.programmers.co.kr/learn/courses/30/lessons/12933
 연습문제 Lv.1
 '''
 
+
 def solution(n):
     lst = []
     while n:
-        n, divmod(n, 10)
+        n, r = divmod(n, 10)
         lst.append(r)
-            lst.sort(reverse=TrueD)
-        for el in lst: 
+        lst.sort(reverse=True)
+        answer = 0
+        for el in lst:
             answer *= 10
             answer += el
-
-    return 
+    return answer
