b7350a059b5e1aae402bbe7f3f24f7d7774336fe
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-11-05 19:25:47 +0900

    TIL Day8 List Comprehension 으로 개선

--- PGR176963.py
+++ PGR176963.py
@@ -6,14 +6,15 @@ def solution(name, yearning, photo):
 
     for n, y in zip(name, yearning):
         name2yearning[n] = y
-    answer = []
-    for p in photo:
-        # score = 0
-        # for name in p:
-        score = sum(name2yearning[name] for name in p)
-        # if name in name2yearning:
-        #     score += name2yearning[name]
-        answer.append(score)
-    return answer
+    return [sum(name2yearning[name] for name in p) for p in photo]
+    # answer = []
+    # for p in photo:
+    #     # score = 0
+    #     # for name in p:
+    #     score = sum(name2yearning[name] for name in p)
+    #     # if name in name2yearning:
+    #     #     score += name2yearning[name]
+    #     answer.append(score)
+    # return answer
 
 # 1. name 이랑 yearning 이 1:1 로 맵핑 -> name2yearning
