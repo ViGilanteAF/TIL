b519587dc18e46ab035213a209fae27105b23eb6
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2023-11-10 08:03:41 +0900

    TIL Day 17

---
+++ LEET46.py
@@ -0,0 +1,12 @@
+# Permutation
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def helper(prefix, nums):
+            if not nums:
+                yield prefix
+                return
+            for idx, num in enumerate(nums):
+                yield from helper(prefix + [num], nums[:idx] + nums[idx+1:])
+        return list(helper([], nums))
