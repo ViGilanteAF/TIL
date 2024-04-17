b54a6c4d62d866f202336004dac9e4955a81fc60
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2024-04-17 22:05:22 +0900

    TIL Day 24

---
+++ BOJ24264.py
@@ -0,0 +1,9 @@
+# 시간복잡도 분석 문제 코테 출제 가능성은 매우 낮음!!
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+print(N * N)
+print(2)
---
+++ BOJ24265.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+sum = 0
+
+for i in range(N):
+    sum += i
+print(sum)
+print(2)
