12c17597b6b25ba712e53d4755487771895d2e63
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2024-06-14 01:10:35 +0900

    TIL Day 39

---
+++ BOJ10813.py
@@ -0,0 +1,18 @@
+# swap 하는것에 대하여 알아보는 문제
+n, m = map(int, input().split())
+# a = [i for i in range(1, n+1)]
+
+a = []
+for i in range(1, n+1):
+    a.append(i)
+
+
+for i in range(m):
+    s, e = map(int, input().split())
+    temp = a[s-1]
+    a[s-1] = a[e-1]
+    a[e-1] = temp
+
+
+for i in range(n):
+    print(a[i], end=' ')
