078ee5cb1ea20e501a46dd1e66074ea60438a3d5
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2024-11-30 01:48:24 +0900

    BOJ2309
    BOJ2309 코드 레퍼런스에 맞도록 수정 내용수정을 없고 띄어쓰기만

--- BOJ2309.py
+++ BOJ2309.py
@@ -1,8 +1,9 @@
 import sys
+
 input = sys.stdin.readline
 arr = []
 total = 0
-first =0
+first = 0
 second = 0
 
 for i in range(9):
@@ -10,10 +11,9 @@ for i in range(9):
     arr.append(n)
     total = sum(arr)
 
-
 for i in range(8):
-    for j in range(i+1,9):
-        if total - (arr[i]+arr[j]) == 100:
+    for j in range(i + 1, 9):
+        if total - (arr[i] + arr[j]) == 100:
             first, second = arr[i], arr[j]
             break
 arr.sort()
