909b072da58e09f9dfc4e215e26b51b3f44af13d
HU SANG MIN <40601526+ViGilanteAF@users.noreply.github.com>
2025-07-22 02:07:35 +0900

    TIL 71 Fix
    BOJ 1181 상단 풀이 과정 추가

--- BOJ1181.py
+++ BOJ1181.py
@@ -1,6 +1,7 @@
-import sys
-# input = sys.stdin.readline
-
+'''
+배열에 문자열 갯수대로 나눠서 저장후 문자열 갯수가 가장 적은수 부터 출력
+[{a},{ab},{abc},{abcd}] 이런식으로...
+'''
 n = int(input())
 arr = [set() for _ in range(20001)]
 for _ in range(n):
